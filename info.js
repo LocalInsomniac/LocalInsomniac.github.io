9233977e65f97f49d6b174909f5d79eaec7b936e
Can't Sleep <localamnesiac@gmail.com>
2024-03-29 05:13:51 +0200

    WIP character misc pages

--- info.js
+++ info.js
@@ -1,21 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 	document.getElementById("lastUpdated").appendChild(document.createTextNode(document.lastModified));
-	
-	document.querySelectorAll("img[modal]").forEach((img) => {
-		var content = document.getElementById("modal-preview");
-		
-		img.addEventListener("click", () => {
-			content.style.opacity = "1";
-			content.style.zIndex = "100";
-			document.documentElement.style.overflow = "hidden";
-			content.innerHTML = "";
-			content.appendChild(new Image()).src = img.src;
-		});
-		
-		content.addEventListener("click", () => {
-			content.style.opacity = "";
-			content.style.zIndex = "";
-			document.documentElement.style.overflow = "";
-		});
-	});
 });
