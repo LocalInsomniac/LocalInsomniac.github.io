3ae3cc7bbfb27fd05facae8e774ccf0198a5b7dc
Can't Sleep <localamnesiac@gmail.com>
2024-03-29 21:13:16 +0200

    broken modal test

--- modal.js
+++ modal.js
@@ -1,6 +1,7 @@
 const modalPopup = document.createElement("div");
 
 modalPopup.id = "modal-popup";
+modalPopup.style.cursor = "zoom-out";
 
 modalPopup.onclick = () => {
 	modalPopup.textContent = "";
@@ -13,23 +14,25 @@ const modals = document.getElementsByClassName("modal");
 
 for (const modal of modals) {
 	modal.addEventListener("click", makeShowModal(modal));
+	modal.style.cursor = "zoom-in";
 }
 
 function makeShowModal(modal) {
 	return (ev) => {
-    		modalPopup.className = "up";
+    	modalPopup.className = "up";
 
-    		const biggie = document.createElement(modal.tagName);
-
+		const biggie = document.createElement(modal.tagName);
+		
 		for (const attr of modal.attributes) {
-    			if (attr.name.startsWith("data-modal-")) {
-        			// TODO TODO TODO.
-        			const special = attr.name.split("data-modal-", 1)[1];
-    			} else if (attr.name != "class") {
-        			biggie.attributes.setNamedItem(attr.cloneNode());
-    			}
+    		if (attr.name.startsWith("data-modal-")) {
+        		// TODO TODO TODO.
+        		const special = attr.name.split("data-modal-", 1)[1];
+    		} else if (attr.name != "class") {
+        		biggie.attributes.setNamedItem(attr.cloneNode());
+			}
 		}
 
-    		modalPopup.appendChild(biggie);
+		biggie.style.cursor = "";
+		modalPopup.appendChild(biggie);
 	};
 }
