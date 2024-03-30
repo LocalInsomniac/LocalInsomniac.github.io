728c77aad9120178c9bb7831bf8dd920fbfce5a2
Can't Sleep <localamnesiac@gmail.com>
2024-03-30 03:05:39 +0200

    drum and cleanup

--- info.js
+++ info.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function (event) { 
 	document.getElementById("lastUpdated").appendChild(document.createTextNode(document.lastModified));
+
+	let gang = document.getElementById("gang");
+	const d = new Date();
+
+	switch (d.getDay() % 2) {
+		case 0:
+			gang.src = "/assets/gang_hunk.png";
+
+			break;
+		
+		case 1:
+			gang.src = "/assets/gang_gilbert.png";
+			gang.style.transform = "translateY(-25%)";
+
+			break;
+	}
+	
 });
