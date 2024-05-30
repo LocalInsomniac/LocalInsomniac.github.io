fbdc1e1f2887cd47b2175a54f3f69163e33bb0d5
Can't Sleep <localamnesiac@gmail.com>
2024-05-30 05:31:43 +0300

    Site cleanup

--- info.js
+++ info.js
@@ -1,23 +1,3 @@
 document.addEventListener("DOMContentLoaded", function (event) { 
 	document.getElementById("lastUpdated").appendChild(document.createTextNode(document.lastModified));
-
-	let gang = document.getElementById("gang");
-	const d = new Date();
-
-	switch (d.getHours() % 3) {
-		case 0:
-			gang.src = "/assets/gang_hunk.png";
-
-			break;
-		
-		case 1:
-			gang.src = "/assets/gang_gilbert.png";
-
-			break;
-		
-		case 2:
-			gang.src = "/assets/gang_video.png";
-
-			break;
-	}
 });
