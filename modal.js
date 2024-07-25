df091f6905a8ec00401f0bb7e4fd864a0fd148bd
Can't Sleep <localamnesiac@gmail.com>
2024-07-25 12:56:05 +0300

    Under the hood changes

--- modal.js
+++ modal.js
@@ -23,26 +23,43 @@ function makeShowModal(modal) {
 		modalPopup.style.cursor = "zoom-out";
 
 		let biggie;
-		let video = modal.dataset.modalVideo;
+		let dataset = modal.dataset;
+		let image = dataset.modalImage;
 
-		if (video) {
-			biggie = document.createElement("video");
-			biggie.src = video;
-			biggie.autoplay = true;
-			biggie.controls = true;
-			biggie.style.cursor = "auto";
+		if (image) {
+			biggie = document.createElement("img");
+			biggie.src = image;
 		} else {
-			biggie = document.createElement(modal.tagName);
-			
-			for (const attr of modal.attributes) {
-				if (!(attr.name.startsWith("data-modal-") || attr.name == "class")) {
-					biggie.attributes.setNamedItem(attr.cloneNode());
-				}
-			}
+			let video = dataset.modalVideo;
 
-			biggie.style.cursor = "zoom-out";
+			if (video) {
+				biggie = document.createElement("video");
+				biggie.src = video;
+				biggie.autoplay = true;
+				biggie.controls = true;
+				biggie.style.cursor = "auto";
+			} else {
+				biggie = document.createElement(modal.tagName);
+				
+				for (const attr of modal.attributes) {
+					if (!(attr.name.startsWith("data-modal-") || attr.name == "class")) {
+						biggie.attributes.setNamedItem(attr.cloneNode());
+					}
+				}
+
+				biggie.style.cursor = "zoom-out";
+			}
 		}
 
 		modalPopup.appendChild(biggie);
+
+		let text = dataset.modalText;
+
+		if (text) {
+			let para = document.createElement("p");
+			
+			para.textContent = text;
+			modalPopup.appendChild(para);
+		}
 	};
 }
