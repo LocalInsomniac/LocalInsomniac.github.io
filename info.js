d0ec53f016999755e4325a0c6d04035be20cbaa0
Can't Sleep <localamnesiac@gmail.com>
2025-03-26 03:12:12 +0200

    Cleanup

--- info.js
+++ info.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-	document.getElementById("lastUpdated").appendChild(document.createTextNode(document.lastModified));
+	document.getElementById("lastUpdated").appendChild(document.createTextNode("Last updated: " + document.lastModified));
 });
